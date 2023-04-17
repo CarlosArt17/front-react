2158aa9ecc7c64ce2871fa1aa70a5eda68752556
Carlos Arturo <zs20006730@estudiantes.uv.mx>
2023-04-17 09:10:44 -0600

    crear props

--- src/App.js
+++ src/App.js
@@ -5,24 +5,28 @@ import Characters from './components/Characters';
 
 function App() {
   const [characters, setCharacteres] = useState(null);
+
   const reqApi = async () => {
     const api = await fetch('https://rickandmortyapi.com/api/character');
     const characterApi = await api.json();
-    console.log(characterApi);
+
     setCharacteres(characterApi.results);
   };
-
-  console.log(characters);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1 className='title'>Rick & Morty</h1>
-        <Characters />
-        <img src={imageRickMorty} alt="Rick & Morty" className="img-home"></img>
-        <button onClick={reqApi} className='btn-search'>
-          Buscar Personajes
-        </button>
+        {characters ? (
+          <Characters characters={characters} />
+        ) : (
+          <>
+          <img src={imageRickMorty} alt="Rick & Morty" className="img-home"></img>
+            <button onClick={reqApi} className='btn-search'>
+              Buscar Personajes
+            </button>
+          </>
+        )}   
       </header>
     </div>
   );
