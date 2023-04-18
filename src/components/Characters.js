a5745b68c1e75d9e8787bca659a0d8abb5a3f753
Carlos Arturo <zs20006730@estudiantes.uv.mx>
2023-04-18 01:20:51 -0600

    nombres de personajes y estados

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -3,13 +3,34 @@ export default function Characters(props) {
     
     return (
         <div className="characters">
-
             <h1>Personajes</h1>
             <span className="back-home">Volver al inicio</span>
             <div className="container-characters">
                 {characters.map((character, index) => (
                     <div className="character-container" key={index}>
-                        <p>{character.name}</p>
+                        <div>
+                            <img src={character.image} alt={character.name} />
+                        </div>
+                        <div>
+                            <h3>{character.name}</h3>
+                            <h6>
+                                {character.status === "Alive" ? (
+                                <>
+                                    <span className="alive" />
+                                    Alive
+                                </>
+                                ) : (
+                                    <>
+                                        <span className="dead" />
+                                        Dead
+                                    </>
+                                )}
+                            </h6>
+                            <p>
+                                <span className="text-grey">Episodios:</span>
+                                <span>{character.episode.length}</span>
+                            </p>
+                        </div>
                     </div>
                 ))}
             </div>
